8214fcd7cfdc71432946ce47683d6d309147a2f1
wisdom-weaver <danish.ansari.nox@gmail.com>
2021-01-29 07:23:44 +0530

    injuries reformat

--- src/views/InjuriesPage.js
+++ src/views/InjuriesPage.js
@@ -6,77 +6,69 @@ import SmallLogo from '../components/SmallLogo';
 import { get_sheet_url, get_team_data } from '../utils/utils'
 
 const sheet_id = "1cUcZSRXi5ksKsHqTnQGTtWkhflNbxUpTTwaPmLv-cmk";
-const sheet_no = "6";
+const sheet_no = "7";
 
 const injuries_key_mapping = [
   {
-    "key_init": "gsx$teams",
-    "key_final": "teams",
-    "key_head": "Teams"
+    key_head: "Team",
+    key_init: "gsx$teamid",
+    key_final: "team",
   },
   {
-    "key_init": "gsx$player",
-    "key_final": "player",
-    "key_head": "Player"
+    key_head: "Team",
+    key_init: "gsx$team",
+    key_final: "teams",
   },
   {
-    "key_init": "gsx$pos",
-    "key_final": "pos",
-    "key_head": "Pos"
+    key_head: "Player",
+    key_init: "gsx$player",
+    key_final: "player",
   },
   {
-    "key_init": "gsx$date",
-    "key_final": "date",
-    "key_head": "Date"
+    key_head: "Position",
+    key_init: "gsx$position",
+    key_final: "position",
   },
   {
-    "key_init": "gsx$injury",
-    "key_final": "injury",
-    "key_head": "Injury"
+    key_head: "Updated",
+    key_init: "gsx$updated",
+    key_final: "updated",
   },
   {
-    "key_init": "gsx$status",
-    "key_final": "status",
-    "key_head": "Status"
+    key_head: "Injury",
+    key_init: "gsx$injury",
+    key_final: "injury",
   },
   {
-    "key_init": "gsx$expectedreturn",
-    "key_final": "expectedreturn",
-    "key_head": "Expected Return"
+    key_head: "Injury Status",
+    key_init: "gsx$injurystatus",
+    key_final: "injurystatus",
   },
-  {
-    "key_init": "gsx$comments",
-    "key_final": "comments",
-    "key_head": "Comments"
-  },
-  {
-    "key_init": "gsx$team",
-    "key_final": "team",
-    "key_head": "Team"
-  }
 ]
 
-const structure_injuries_raw_data = ({raw, search}) => {
+const structure_injuries_raw_data = ({ raw, search }) => {
   var structured = {};
-  if (search?.length>0){
-    const filter = raw.filter(row=>
+  if (search?.length > 0) {
+    const filter = raw.filter(row =>
       row.content.$t
-      .replace(' ','')
-      .replace('player','')
-      .replace('pos','')
-      .replace('date','')
-      .replace('injury','')
-      .replace('status','')
-      .replace('expectedreturn','')
-      .replace('comments','')
-      .toLowerCase().includes(search.toLowerCase()));
+        .replace(' ', '')
+        .replace('player', '')
+        .replace('pos', '')
+        .replace('date', '')
+        .replace('injury', '')
+        .replace('status', '')
+        .replace('expectedreturn', '')
+        .replace('comments', '')
+        .replace(':', '')
+        .toLowerCase()
+      .includes(search.toLowerCase()));
     raw = filter;
   }
 
 
   for (var row of raw) {
     row = injuries_key_mapping.reduce((acc, { key_init, key_final, key_head }) => ({ ...acc, [key_final]: row[key_init]?.$t }), {})
-    
+
     var team = row['team']
     structured[team] = {
       ...(structured[team] || []),
@@ -97,39 +89,39 @@ const EachTeamInjuries = ({ team_ob }) => {
   return (
     <>
       <div className="card">
-      <div className="card-content">
-        <div className="row-flex flex-start">
-          <LargeLogo image={teamImg} />
-          <h5 className="bold center">{teamName}</h5>
-        </div>
-        <div className="spacing-10px"></div>
+        <div className="card-content">
+          <div className="row-flex flex-start">
+            <LargeLogo image={teamImg} />
+            <h5 className="bold center">{teamName}</h5>
+          </div>
+          <div className="spacing-10px"></div>
 
-        {injuries && injuries.length != 0 ? (
-          injuries?.map((inj) => (
-            <div>
-              <div className="row-flex head justify-center">
-              <span>Player: </span> 
-              <div className="m5"><SmallLogo image={teamImg}/></div>
-              <span className="flow-text">{inj['player']}</span>
-              </div>
-              <div className="row-flex space-between left-align wrap">
-                {injuries_key_mapping.slice(2, 7).map(({ key_head, key_final }) => (
-                  <div className="col-flex mh-60px align-flex-start justify-flex-start m5">
-                    <p className="head">{key_head}</p>
-                    <p>{inj[key_final]}</p>
-                  </div>
-                ))}
-              </div>
-              <div className="spacing-5px"></div>
-              <p className="left-align">{inj["comments"]}</p>
-              <div className="spacing-10px"></div>
-              <div className="spacing-20px"></div>
-            </div>
-          ))
-        ) : (
-            <h6 className="center head">No Injuries</h6>
-          )}
-      </div>
+          {injuries && injuries.length != 0 ? (
+            <>
+              <table>
+                <tbody>
+                  <tr>
+                    {injuries_key_mapping.slice(2).map(({ key_head }) => (
+                      <th>{key_head}</th>
+                    ))}
+                  </tr>
+                  {injuries?.map((inj) => (
+                    <>
+                      <tr>
+                        <th>{inj['player']}</th>
+                        {injuries_key_mapping.slice(3).map(({ key_final }) => (
+                          <td>{inj[key_final]}</td>
+                        ))}
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          ) : (
+              <h6 className="center head">No Injuries</h6>
+            )}
+        </div>
       </div>
     </>
   )
@@ -140,18 +132,18 @@ const InjuriesJSX = (props) => {
   const { api_data } = props;
   const raw = api_data.feed.entry;
   console.log('injuries jsx raw=>', raw);
-  
+
   const [search, set_search] = useState('');
 
-  const structured = structure_injuries_raw_data({raw, search});
+  const structured = structure_injuries_raw_data({ raw, search });
   console.log('injuries jsx strucured=>', structured);
-  
+
 
   return (
     <div className="">
       <h5 className="center">Injuries JSX</h5>
       <div className="center">
-        <input onChange={(e)=>{set_search(e.target.value)}} type="text" value={search} placeholder="Search"/>
+        <input onChange={(e) => { set_search(e.target.value) }} type="text" value={search} placeholder="Search" />
       </div>
       {Object.keys(structured).map(team =>
         <>
